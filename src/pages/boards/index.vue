4c8d44c5f39acc7a43818885d14ef5bbea8dbad6
Yo-Tina Verbraecken <64355830+iheartcore@users.noreply.github.com>
2022-07-13 16:58:02 +0200

    Exercise 1

---
+++ src/pages/boards/index.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const boards = ref([
+  {
+    id: 1,
+    title: "My First Board",
+    order: [],
+    image: {
+      downloadUrl: "https://picsum.photos/480/270?board=1",
+    },
+  },
+  {
+    id: 2,
+    title: "My Second Board",
+    order: [],
+    image: {
+      downloadUrl: "https://picsum.photos/480/270?board=2",
+    },
+  },
+  {
+    id: 3,
+    title: "My Third Board",
+    order: [],
+  },
+]);
+</script>
+
+<template>
+  <h1 class="text-3xl mb-5">Boards</h1>
+  <div class="flex">
+    <BoardCard v-for="board in boards" :key="board.id" :board="board" />
+    <button class="text-gray-500" @click="createBoard(newBoardTemplate)">
+      <span>New Board +</span>
+    </button>
+  </div>
+</template>
+
+<style scoped></style>
