96a0cf683128703551ed2debc694de8f10f259f7
Yo-Tina Verbraecken <64355830+iheartcore@users.noreply.github.com>
2022-07-14 18:30:20 +0200

    Excercise 12

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 import { provide } from "vue";
+import { useScriptTag } from "@vueuse/core";
 import { DefaultApolloClient } from "@vue/apollo-composable";
 import apolloClient from "@/graphql/ApolloClient";
 
 // Provide Apollo Client to all Vue Components
 provide(DefaultApolloClient, apolloClient);
+
+// Initialize TwicPic
+useScriptTag(`${import.meta.env.VITE_TWICPICS_URL}?v1`);
 </script>
 
 <template>
